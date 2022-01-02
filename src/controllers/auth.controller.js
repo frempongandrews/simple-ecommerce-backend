f6945c4ed898b2e27a28ced7a0b35aa2ea4294f8
Andrews Frempong <frempongandrews@yahoo.com>
2022-01-02 00:50:23 +0000

    not setting access_token on login in progress...

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -210,7 +210,7 @@ export const loginUser = async (req, res) => {
           const jwtToken = existingUser.generateJWT();
           const isDev = process.env.NODE_ENV === "development";
           console.log("**********Setting cookie");
-          res.cookie(keys.cookie.cookieName, jwtToken, { domain: isDev? process.env.DEV_CLIENT_APP_URL : process.env.PROD_CLIENT_APP_URL, httpOnly: true, maxAge: keys.cookie.cookieMaxAge, sameSite: "none", secure: true });
+          res.cookie(keys.cookie.cookieName, jwtToken, { domain: isDev? process.env.DEV_CLIENT_APP_URL : process.env.PROD_CLIENT_APP_URL, httpOnly: process.env.NODE_ENV === "development", maxAge: keys.cookie.cookieMaxAge, sameSite: "none", secure: process.env.NODE_ENV === "development" });
           return res.json({
             message: "Successfully logged in",
             user: existingUser.getUserSummary(),
